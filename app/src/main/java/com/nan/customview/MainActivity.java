9a3f3aee55f0e5e9d762d5a8ba94cad19bc020a1
wangyannan <wangyannan@sagereal.com>
2018-03-08 22:09:46 +0800

    自定义ToggleView
    Change-Id: Idb5e51729975e68b080df6851d74ea8366505a61

--- app/src/main/java/com/nan/customview/MainActivity.java
+++ app/src/main/java/com/nan/customview/MainActivity.java
@@ -2,12 +2,32 @@ package com.nan.customview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.nan.customview.view.ToggleView;
 
 public class MainActivity extends AppCompatActivity {
+    ToggleView mToggleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToggleView = findViewById(R.id.tv_toggle);
+
+        //采用自定义属性替代
+//        mToggleView.setToggleBackgroundResource(R.drawable.switch_background);
+//        mToggleView.setToggleSlideResource(R.drawable.slide_button);
+//        mToggleView.setToggle(true);
+        mToggleView.setOnToggleChangeListener(new ToggleView.OnToggleChangeListener() {
+            @Override
+            public void onToggleChange(boolean open) {
+                if (open) {
+                    Toast.makeText(MainActivity.this, "toogle open", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "toogle close", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
